db3bcc17cf195420a649af60b0102f84f1f3aeb5
Alejandro <estiven9401@gmail.com>
2020-04-01 22:50:19 -0500

    second commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+var http = require("http");
+var log  = require("./modules/log");
+var consts = require("./utils/cons");
+var firebase = require("../libs")
+
+var server = http.createServer(function(request, response){
+    
+    if(request.url === "/"){
+        response.writeHead(200, {"Content-Tipe": "text/html"});
+        response.write("<html><body>Home Page</body></html>");
+        response.end();
+    }else if(request.url === "/exit"){
+        response.writeHead(200, {"Content-Tipe": "text/html"});
+        response.write("<html><body>BYE</body></html>");
+        response.end();
+    }else {
+        response.writeHead(404, {"Content-Tipe": "text/html"});
+        response.write("<html><body>NOT FOUND</body></html>");
+        response.end();
+    }
+
+});
+
+server.listen(4000);
+
+console.log("Running on 4000")
